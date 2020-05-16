37a5539cc0df31360c8514fc4b83d126bda4f3b4
Melvin Perdomo <Melvin Perdomo@MELVIN>
2020-05-16 12:20:10 -0600

    se completaron palabras (100)

--- Form1.cs
+++ Form1.cs
@@ -26,17 +26,30 @@ namespace Ahorcado
         }
         private void bSeleccionar_Click(object sender, EventArgs e)
         {
-            
             try
             {
-
-                int NumeroElemento = 1; // GetRandomNumber(0.0, Palabras.Count());
+                int NumeroElemento = GetRandomNumber(0.0, Palabras.Count());
                 string SelectedWord = Palabras[NumeroElemento].ToString();
                 int Size = SelectedWord.Length;
                 switch (Size)
                 {
+                    case 1:
+                        p1.Visible = true;
+                        break;
+                    case 2:
+                        p1.Visible = true;
+                        p2.Visible = true;
+                        break;
+                    case 3:
+                        p1.Visible = true;
+                        p2.Visible = true;
+                        p3.Visible = true;
+                        break;
                     case 4:
                         p1.Visible = true;
+                        p2.Visible = true;
+                        p3.Visible = true;
+                        p4.Visible = true;
                         break;
                     case 5:
                         p1.Visible = true;
@@ -45,6 +58,57 @@ namespace Ahorcado
                         p4.Visible = true;
                         p5.Visible = true;
                         break;
+                    case 6:
+                        p1.Visible = true;
+                        p2.Visible = true;
+                        p3.Visible = true;
+                        p4.Visible = true;
+                        p5.Visible = true;
+                        p6.Visible = true;
+                        break;
+                    case 7:
+                        p1.Visible = true;
+                        p2.Visible = true;
+                        p3.Visible = true;
+                        p4.Visible = true;
+                        p5.Visible = true;
+                        p6.Visible = true;
+                        p7.Visible = true;
+                        break;
+                    case 8:
+                        p1.Visible = true;
+                        p2.Visible = true;
+                        p3.Visible = true;
+                        p4.Visible = true;
+                        p5.Visible = true;
+                        p6.Visible = true;
+                        p7.Visible = true;
+                        p8.Visible = true;
+                        break;
+                    case 9:
+                        p1.Visible = true;
+                        p2.Visible = true;
+                        p3.Visible = true;
+                        p4.Visible = true;
+                        p5.Visible = true;
+                        p6.Visible = true;
+                        p7.Visible = true;
+                        p8.Visible = true;
+                        p9.Visible = true;
+                        break;
+                    case 10:
+                        p1.Visible = true;
+                        p2.Visible = true;
+                        p3.Visible = true;
+                        p4.Visible = true;
+                        p5.Visible = true;
+                        p6.Visible = true;
+                        p7.Visible = true;
+                        p8.Visible = true;
+                        p9.Visible = true;
+                        p10.Visible = true;
+                        break;
+
                 }
 
 
